2d4630bbea7419128b476a615ffd2d8eb162fb94
cuzai <cuzai@naver.com>
2019-12-18 11:33:14 +0900

    Update all - tidy up some codes

--- Dart_Crawl.py
+++ Dart_Crawl.py
@@ -3,7 +3,8 @@ from lib.Cur_data import Cur_data
 import sqlite3
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-class Dart_Crawl() :
+
+class Dart_Crawl:
     def __init__(self):
         self.comp_list = self.read_xlsx()
         db = self.set_db()
@@ -11,48 +12,47 @@ class Dart_Crawl() :
         self.c = db[1]
 
     def set_db(self):
-        conn = sqlite3.connect('./data/data.db', check_same_thread=False)
+        conn = sqlite3.connect("./data/data.db", check_same_thread=False)
         c = conn.cursor()
         c.execute("CREATE TABLE IF NOT EXISTS data(comp text, report text, link text)")
         return conn, c
 
     def read_xlsx(self):
-        wb = openpyxl.load_workbook('./res/종목리스트_통합_19.08.18.xlsx')
+        wb = openpyxl.load_workbook("./res/종목리스트_통합_19.08.18.xlsx")
         ws = wb.active
         comp_list = []
         for r in ws.rows:
             comp_list.append(r[2].value)
         return comp_list
 
-    def save_db(self, cur_first_comp, cur_first_report, cur_first_link) :
-        # self.c.execute('INSERT INTO data VALUES(?, ?)', (current_first_comp,current_first_report))
-        self.c.execute('UPDATE data SET comp = ?', (cur_first_comp,))
-        self.c.execute('UPDATE data SET report = ?', (cur_first_report,))
-        self.c.execute('UPDATE data SET link = ?', (cur_first_link,))
+    def save_db(self, cur_first_comp, cur_first_report, cur_first_link):
+        self.c.execute("UPDATE data SET comp = ?", (cur_first_comp,))
+        self.c.execute("UPDATE data SET report = ?", (cur_first_report,))
+        self.c.execute("UPDATE data SET link = ?", (cur_first_link,))
         self.conn.commit()
 
     def get_db(self):
-        comp = self.c.execute('SELECT comp FROM data')
+        comp = self.c.execute("SELECT comp FROM data")
         comp = self.c.fetchall()[0][0]
 
-        report = self.c.execute('SELECT report FROM data')
+        report = self.c.execute("SELECT report FROM data")
         report = self.c.fetchall()[0][0]
 
-        link = self.c.execute('SELECT link FROM data')
+        link = self.c.execute("SELECT link FROM data")
         link = self.c.fetchall()[0][0]
 
         return comp, report, link
 
     def is_verified(self, comp_name):
-        if comp_name in self.comp_list :
+        if comp_name in self.comp_list:
             return True
 
     def compare(self, db_link, cur_link):
-        if db_link == cur_link :
+        if db_link == cur_link:
             return True
 
     def append_result(self, result, comp_name, report, link):
-        result.append({'comp_name': comp_name, 'report': report, 'link': link})
+        result.append({"comp_name": comp_name, "report": report, "link": link})
 
     def main(self):
         result = []
@@ -63,26 +63,26 @@ class Dart_Crawl() :
         db_link = db[2]
 
         cd = Cur_data()
-        while(idx < 100) :
+        while idx < 100:
             cur_data = cd.get_cur_data(idx)
             cur_comp = cur_data[0]
             cur_report = cur_data[1]
             cur_link = cur_data[2]
-            if self.is_verified(cur_comp) :
-                if not(self.compare(db_link, cur_link)) :
-                    if is_first :
+            if self.is_verified(cur_comp):
+                if not (self.compare(db_link, cur_link)):
+                    if is_first:
                         self.save_db(cur_comp, cur_report, cur_link)
                         is_first = False
                     self.append_result(result, cur_comp, cur_report, cur_link)
                     idx += 1
-                else :
+                else:
                     return print(result)
-            else :
+            else:
                 idx += 1
         return print(result)
 
-if __name__ == "__main__" :
-    sched = BlockingScheduler()
-    sched.add_job(Dart_Crawl().main, 'cron', second='*/15')
-    sched.start()
 
+if __name__ == "__main__":
+    sched = BlockingScheduler()
+    sched.add_job(Dart_Crawl().main, "cron", second="*/15")
+    sched.start()
--- lib/Cur_data.py
+++ lib/Cur_data.py
@@ -1,12 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
 
-class Cur_data() :
+
+class Cur_data:
     def __init__(self):
-        self.URL = 'http://dart.fss.or.kr/dsac001/mainAll.do'
+        self.URL = "http://dart.fss.or.kr/dsac001/mainAll.do"
         req = requests.get(self.URL)
-        soup = BeautifulSoup(req.content, 'html.parser')
-        self.rows = soup.select('.table_list > table > tr')
+        soup = BeautifulSoup(req.content, "html.parser")
+        self.rows = soup.select(".table_list > table > tr")
 
     def my_trim(self, word):
         word = word.strip().replace("\r", "").replace("\n", "").replace("\t", "")
@@ -14,16 +15,16 @@ class Cur_data() :
         return word
 
     def get_cur_data(self, idx):
-        raw_cur_comp = self.rows[idx].select_one('.nobr1').text
+        raw_cur_comp = self.rows[idx].select_one(".nobr1").text
         cur_comp = self.my_trim(raw_cur_comp)
         # print(cur_comp)
 
-        raw_cur_report = self.rows[idx].select('td')[2].text
+        raw_cur_report = self.rows[idx].select("td")[2].text
         cur_report = self.my_trim(raw_cur_report)
         # print(cur_report)
 
-        href = self.rows[idx].select('a')[1]['href']
+        href = self.rows[idx].select("a")[1]["href"]
         # print(href)
-        link = "".join(['http://dart.fss.or.kr', href])
+        link = "".join(["http://dart.fss.or.kr", href])
 
         return cur_comp, cur_report, link
